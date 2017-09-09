bdaac7954881dd44876f80ee1ffe1778dafecd12
Qi Xi <xqi@thoughtworks.com>
2017-09-09 15:09:09 +0800

    add basic code for spring cloud contract workshop

---
+++ scc-client/src/test/java/radar/client/RadarClientApplicationTests.java
@@ -0,0 +1,28 @@
+package radar.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@AutoConfigureStubRunner(ids = {"radar:scc-server:+:stubs:8080"}, workOffline = true)
+public class RadarClientApplicationTests {
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void shouldGetTechniquesMicroservices() {
+//        @SuppressWarnings("unchecked") final Map<String, String> response = restTemplate.getForObject("http://localhost:8080/techniques/microservices", Map.class);
+//        assertThat(response).contains(entry("quadrant", "Techniques"), entry("ring", "TRIAL"));
+    }
+
+}
---
+++ scc-server/src/test/resources/contracts/shouldGetTechniquesMicroservices.groovy
@@ -0,0 +1,19 @@
+/*
+org.springframework.cloud.contract.spec.Contract.make {
+    request {
+        method 'GET'
+        url '/techniques/microservices'
+    }
+    response {
+        status 200
+        body("""
+  {
+    "quadrant": "Techniques",
+    "ring": "TRIAL"
+  }
+  """)
+        headers {
+            header('Content-Type': 'application/json;charset=UTF-8')
+        }
+    }
+}*/
