53b24041ed87e132f9c778e3b1feaac1c5f1da8c
Qi Xi <xqi@thoughtworks.com>
2017-09-09 15:19:13 +0800

    add basic code for moco and moscow workshop

---
+++ mm-server/src/test/java/radar/server/TechniquesApiTest.java
@@ -0,0 +1,28 @@
+package radar.server;
+
+//import com.github.macdao.moscow.ContractAssertion;
+//import com.github.macdao.moscow.ContractContainer;
+//import com.github.macdao.moscow.model.Contract;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.nio.file.Paths;
+import java.util.List;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TechniquesApiTest {
+//    @LocalServerPort
+//    private int port;
+
+//    @Test
+//    public void can_get_techniques_microservices() throws Exception {
+//        final ContractContainer contractContainer = new ContractContainer(Paths.get("contracts"));
+//        final List<Contract> contractList = contractContainer.findContracts("can_get_techniques_microservices");
+//        new ContractAssertion(contractList).setPort(port)
+//                .assertContract();
+//    }
+}
