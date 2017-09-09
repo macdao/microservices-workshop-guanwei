d2cb4d05b6cf0f9f454f4ecb7fea806d6dcb89e5
Qi Xi <xqi@thoughtworks.com>
2017-09-09 15:09:09 +0800

    add basic code for spring cloud contract workshop

---
+++ scc-client/src/main/java/radar/client/RadarClientApplication.java
@@ -0,0 +1,12 @@
+package radar.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadarClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadarClientApplication.class, args);
+	}
+}
---
+++ scc-server/src/test/java/radar/server/MvcTest.java
@@ -0,0 +1,24 @@
+package radar.server;
+
+//import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = RadarServerApplication.class)
+//@AutoConfigureMockMvc
+public abstract class MvcTest {
+
+//    @Autowired
+//    private MockMvc mockMvc;
+
+//    @Before
+//    public void setup() {
+//        RestAssuredMockMvc.mockMvc(mockMvc);
+//    }
+}
