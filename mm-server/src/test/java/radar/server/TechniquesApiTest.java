e43fa46e6bc1676e12b9734e297339a8a75b7aef
Qi Xi <xqi@thoughtworks.com>
2017-09-09 15:26:55 +0800

    add server test with moscow

--- mm-server/src/test/java/radar/server/TechniquesApiTest.java
+++ mm-server/src/test/java/radar/server/TechniquesApiTest.java
@@ -1,8 +1,8 @@
 package radar.server;
 
-//import com.github.macdao.moscow.ContractAssertion;
-//import com.github.macdao.moscow.ContractContainer;
-//import com.github.macdao.moscow.model.Contract;
+import com.github.macdao.moscow.ContractAssertion;
+import com.github.macdao.moscow.ContractContainer;
+import com.github.macdao.moscow.model.Contract;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
@@ -12,17 +12,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.nio.file.Paths;
 import java.util.List;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TechniquesApiTest {
-//    @LocalServerPort
-//    private int port;
+    @LocalServerPort
+    private int port;
 
-//    @Test
-//    public void can_get_techniques_microservices() throws Exception {
-//        final ContractContainer contractContainer = new ContractContainer(Paths.get("contracts"));
-//        final List<Contract> contractList = contractContainer.findContracts("can_get_techniques_microservices");
-//        new ContractAssertion(contractList).setPort(port)
-//                .assertContract();
-//    }
+    @Test
+    public void can_get_techniques_microservices() throws Exception {
+        final ContractContainer contractContainer = new ContractContainer(Paths.get("contracts"));
+        final List<Contract> contractList = contractContainer.findContracts("can_get_techniques_microservices");
+        new ContractAssertion(contractList).setPort(port)
+                .assertContract();
+    }
 }
